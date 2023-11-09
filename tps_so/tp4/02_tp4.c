f55a2db2ae0a54413b0666858891decf530d95be
CosmoCarpincho <cosmocarpincho@gmail.com>
2023-11-09 08:33:34 -0300

    termino ej 2 tp 4 sin validaciones

--- tps_so/tp4/02_tp4.c
+++ tps_so/tp4/02_tp4.c
@@ -4,11 +4,25 @@ elementos numéricos y los devuelva ordenado de menor a mayor.
 
 #include <stdio.h>
 
+void ordenar_vector(int *vector, int tam_vector) {
+
+  int aux;
+  for (size_t i = 0; i < tam_vector - 1; i++) {
+    for (size_t j = i + 1; j < tam_vector; j++) {
+      if (vector[j] < vector[i]) {
+        aux = vector[i];
+        vector[i] = vector[j];
+        vector[j] = aux;
+      }
+    }
+  }
+}
+
 int main() {
   int vector[10];
 
-  printf("Ingrese 10 numeros para cargaral vector\n");
-  for (size_t i = 0; i < 10; i++) {
+  printf("Ingrese 10 numeros para cargar al vector\n");
+  for (int i = 0; i < 10; i++) {
     printf("Posición %d:", i + 1);
     scanf("%d", &vector[i]);
   }
@@ -17,7 +31,14 @@ int main() {
 
   printf("\nVector => ");
   for (size_t j = 0; j < tam_vector; j++) {
-    printf("[%d]", vector[j]);
+    printf("%d ", vector[j]);
+  }
+
+  ordenar_vector(vector, tam_vector);
+
+  printf("\nVector ordenado de menor a mayor => ");
+  for (size_t j = 0; j < tam_vector; j++) {
+    printf("%d ", vector[j]);
   }
 
   printf("\n");
