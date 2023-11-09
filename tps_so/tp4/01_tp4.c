2e0273ec80fbd70fea6f17391328ad58c32aba4b
CosmoCarpincho <cosmocarpincho@gmail.com>
2023-11-09 08:18:27 -0300

    correccion de 01_tp4.c arrancaba de 0

--- tps_so/tp4/01_tp4.c
+++ tps_so/tp4/01_tp4.c
@@ -7,13 +7,13 @@ realice una rutina en este lenguaje que sume los 5 primeros números naturales.
 #include <stdio.h>
 
 int main() {
-  int num = 0;
-  int i;
-  for (i = 0; i < 5; i++) {
-    num += i;
-  }
+        int num = 0;
+        int i;
+        for (i = 1; i <= 5; i++) {
+                num += i;
+        }
 
-  printf("La suma de los primeros 5 números naturales es: %d\n", num);
+        printf("La suma de los primeros 5 números naturales es: %d\n", num);
 
-  return 0;
+        return 0;
 }
